a5401ea092bf2c9fbc9390e7283371d66ca30501
jeaninemtn <jeaninemtn@gmail.com>
2025-12-03 08:52:50 +0800

    update pydantic warning: rename orm_mode to from_attributes

--- app/schemas/project_schema.py
+++ app/schemas/project_schema.py
@@ -16,4 +16,4 @@ class ProjectRead(BaseModel):
     user_id: int
 
     class Config:
-        orm_mode = True
+        from_attributes = True
--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -24,7 +24,7 @@ class UserRead(UserBase):
     id: int
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class UserLogin(BaseModel):
     username: str
